2bcfa415436d8e5333edd50660d9bb1552fe405d
Om Hursal <op83846@gmail.com>
2026-03-30 23:18:48 +0530

    set postgres db

--- config/settings.py
+++ config/settings.py
@@ -59,19 +59,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-# --- DATABASE CONFIG ---
-# Use MySQL if DB_HOST is set, else default to SQLite
-# config/settings.py
 _mysql = os.environ.get("DB_HOST")
 if _mysql:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.getenv("DB_NAME"),
             'USER': os.getenv("DB_USER"),
             'PASSWORD': os.getenv("DB_PASSWORD"),
             'HOST': os.getenv("DB_HOST"),
-            'PORT': os.getenv("DB_PORT", "3306"),
+            'PORT': os.getenv("DB_PORT", "5432"),
             'OPTIONS': {
                 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             },
