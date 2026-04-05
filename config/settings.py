66a3672c9467ad6bc1e531551f0a53a95c2a7185
Om Hursal <op83846@gmail.com>
2026-04-05 23:45:50 +0530

    fix connection with postgreSql

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "liferoute",
-        "USER": "postgres",
+        "USER": "op838",
         "PASSWORD": "Omyy@123",
         "HOST": "127.0.0.1",
         "PORT": "5432",
