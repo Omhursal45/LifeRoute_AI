c22908f86890ad857c38454443948351f2456702
Om Hursal <op83846@gmail.com>
2026-04-13 23:23:55 +0530

    fix static serving issue

--- config/settings.py
+++ config/settings.py
@@ -110,8 +110,12 @@ USE_TZ = True
 LOCALE_PATHS = [BASE_DIR / "locale"]
 
 STATIC_URL = "/static/"
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "accounts.User"
