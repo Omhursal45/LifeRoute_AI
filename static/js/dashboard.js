b4f87e8e10c940b9c430b053aa062253ff0b2ce2
Om Hursal <op83846@gmail.com>
2026-05-08 20:44:45 +0530

    Enahance the dashboard

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -57,7 +57,7 @@
       return { ok: false, error: msg };
     }
     try {
-      sessionStorage.setItem(SESSION_ANCHOR_KEY, '2');
+      sessionStorage.setItem(SESSION_ANCHOR_KEY, '1');
     } catch (e) {
       /* ignore */
     }
